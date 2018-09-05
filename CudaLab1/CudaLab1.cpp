0c05a0b8dbc2359b8efeea0b020fafd05c313d55
William Garneau <willi_garn@hotmail.com>
2018-09-05 14:16:20 -0400

    Removed useless dependencies

--- CudaLab1/CudaLab1.cpp
+++ CudaLab1/CudaLab1.cpp
@@ -7,8 +7,6 @@
 
 #include <opencv2/core/core.hpp> 
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/types_c.h> 
-#include <opencv2/imgproc/imgproc.hpp> 
 
 const int PIXEL_INCREMENTATION = 100;
 
